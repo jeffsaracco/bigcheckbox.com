bc3dfeadeb586ee9b88306a19d74fdc2be03ce0f
Jeff Saracco <jeffsaracco@gmail.com>
2014-04-24 20:45:27 -0400

    Don't add blank items

--- bigcheckbox.js
+++ bigcheckbox.js
@@ -3,11 +3,13 @@ $(function(){
     e.preventDefault();
 
     $item = $('#item-text');
-    newItem = $item.val();
-    $item.val('');
-    template = $('#template').html();
-    template = template.replace("#{id}", $('#list li').length);
-    template = template.replace("#{text}", newItem);
-    $('#list').append(template);
+    newItem = $item.val().trim();
+    if(newItem != '') {
+      $item.val('');
+      template = $('#template').html();
+      template = template.replace(/#{id}/g, $('#list li').length);
+      template = template.replace("#{text}", newItem);
+      $('#list').append(template);
+    }
   });
 });
