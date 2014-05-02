b575845752e8956e33c75645509bb8582d4fefd7
Edina Vath <Edina.Vath@gmail.com>
2014-05-02 07:42:10 -0400

    change images/styles and reduce font size for longer texts

--- bigcheckbox.js
+++ bigcheckbox.js
@@ -7,9 +7,19 @@ $(function(){
     if(newItem != '') {
       $item.val('');
       template = $('#template').html();
-      template = template.replace(/#{id}/g, $('#list li').length);
+      listLength = $('#list li').length;
+      template = template.replace(/#{id}/g, listLength);
+      template = template.replace(/#{li_id}/g, listLength);
       template = template.replace("#{text}", newItem);
       $('#list').append(template);
+      size = 1000/newItem.length;
+      if (size > 95){ size = 95; }
+      if (size < 30 ) {size = 35; }
+      $('#li-' + listLength).css("font-size", size );
     }
+  });
+
+  $('div').on("click", function(e){
+
   });
 });
