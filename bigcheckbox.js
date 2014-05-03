7d1de7e8019403bdc9556f834fc541dc98daeffd
Edina Vath <Edina.Vath@gmail.com>
2014-05-03 09:42:46 -0400

    adjust footer, check box when text is clicked

--- bigcheckbox.js
+++ bigcheckbox.js
@@ -16,10 +16,30 @@ $(function(){
       if (size > 95){ size = 95; }
       if (size < 30 ) {size = 35; }
       $('#li-' + listLength).css("font-size", size );
+      if (listLength >= 4){ adjustFooter(); }
     }
   });
+});
 
-  $('div').on("click", function(e){
-
+$(function(){
+  $('ul').click(function(event){
+    var id = event.target.id;
+    if ($('#'+id).prop("checked")){
+      $('#'+id).prop("checked", false)
+    } else {
+      $('#'+id).prop("checked", true);
+    }
   });
 });
+
+function adjustFooter(){
+  var bottom = $('footer').css("bottom")
+  bottom = bottom.substring(0, bottom.length - 2);
+  bottom = parseInt(bottom, 10);
+  bottom -= 104;
+  bottom = bottom.toString()+"px";
+  $('footer').css("bottom", bottom)
+};
+
+
+
