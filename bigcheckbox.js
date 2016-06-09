f1c4ccb1a4a40c7aeac04d38cc3d58df25dad0d2
Jeff Saracco <jeffsaracco@gmail.com>
2016-06-09 09:48:35 -0400

    Add saving of items to localstorage and add clear button

--- bigcheckbox.js
+++ bigcheckbox.js
@@ -1,3 +1,25 @@
+var KEY = "bigcheckbox";
+var items = localStorage.getItem(KEY) ? JSON.parse(localStorage.getItem(KEY)) : [];
+
+function addItem(item) {
+  var template = $('#template').html();
+  var listLength = $('#list li').length;
+  template = template.replace(/#{id}/g, listLength);
+  template = template.replace(/#{li_id}/g, listLength);
+  template = template.replace("#{text}", item.label);
+
+  $('#list').append(template);
+
+  if(item.checked) {
+    $("#checkbox-" + listLength).prop("checked", true);
+  }
+
+  var size = 1000/item.label.length;
+  if (size > 95){ size = 95; }
+  if (size < 30 ) {size = 35; }
+  $('#li-' + listLength).css("font-size", size );
+}
+
 $(function(){
   $('#new-item').on("submit", function(e){
     e.preventDefault();
@@ -6,44 +28,34 @@ $(function(){
     newItem = $item.val().trim();
     if(newItem != '') {
       $item.val('');
-      template = $('#template').html();
-      listLength = $('#list li').length;
-      template = template.replace(/#{id}/g, listLength);
-      template = template.replace(/#{li_id}/g, listLength);
-      template = template.replace("#{text}", newItem);
-      $('#list').append(template);
-      size = 1000/newItem.length;
-      if (size > 95){ size = 95; }
-      if (size < 30 ) {size = 35; }
-      $('#li-' + listLength).css("font-size", size );
-      if (listLength >= 4){ adjustFooter(); }
+      var item = {label: newItem, checked: false};
+      addItem(item);
+      items.push(item);
+      localStorage.setItem(KEY, JSON.stringify(items));
     }
   });
-});
 
-$(function(){
   $('ul').click(function(event){
     event.preventDefault();
     var id = event.target.id;
-    if (id ==="" ){ id = event.target.parentElement.id; }
-    if ($('#'+id).prop("checked")){
-      $('#'+id).prop("checked", false)
-    } else {
-      $('#'+id).prop("checked", true);
-    }
+    id = id.match(/-([0-9])*/)[1];
+
+    var checked = !$('#checkbox-'+id).prop("checked");
+
+    items[parseInt(id)].checked = checked;
+    localStorage.setItem(KEY, JSON.stringify(items));
+
+    $('#checkbox-'+id).prop("checked", checked)
+  });
+
+  $('.clear').click(function(event) {
+    localStorage.removeItem(KEY);
+    $('#list li').remove();
+  });
+
+  items.forEach(function(item) {
+    addItem(item);
   });
 
   $('#item-text').focus()
 });
-
-function adjustFooter(){
-  var bottom = $('footer').css("bottom")
-  bottom = bottom.substring(0, bottom.length - 2);
-  bottom = parseInt(bottom, 10);
-  bottom -= 104;
-  bottom = bottom.toString()+"px";
-  $('footer').css("bottom", bottom)
-};
-
-
-
